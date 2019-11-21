28640b50674d9739e037d464ede29106bc9b88f8
km02615 <45234871+km02615@users.noreply.github.com>
2019-11-21 11:29:23 -0500

    still cleaning damit

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+			function init() {
+				
+			  window.addEventListener('scroll', function (e) {
+			    var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+			      shrinkOn = 300,
+			      header = document.querySelector("header");
+			    if (distanceY > shrinkOn) {
+			      classie.add(header, "smaller");
+			    } else {
+			      if (classie.has(header, "smaller")) {
+			        classie.remove(header, "smaller");
+			      }
+			    }
+			  });
+			}
+			$.ajax({
+			  method: 'GET',
+			  url: 'assets/data/menu.json',
+			  dataType: 'json',
+			  success: function (data) {
+				  
+			 
+				  
+			if  (data.menu.length > 0) {
+			  data.menu.forEach(function (data) {
+				  
+			 				
+				console.log(data.menuName);
+			    console.log(data.menuLink);
+				  
+				  $('nav').append('<a href="' + data.MenuLink + '"> ' + data.MenuName + '</nav>');
+			  
+			});
+			}
+			  },
+			error: function() {
+			  console.log('all not good');
+			}
+            });
+			
+			window.onload = init();
+        
